b174f019b136cf4866042b634382ca810f73225f
Stephen M. Coakley <me@stephencoakley.com>
2019-12-11 23:04:43 -0600

    wip

--- src/response.rs
+++ src/response.rs
@@ -1,10 +1,14 @@
 use crate::io::Text;
 use crate::{Metrics, Error};
+use crate::task::Join;
 use futures_io::AsyncRead;
+use futures_util::AsyncReadExt;
 use http::{Response, Uri};
 use std::fs::File;
 use std::io::{self, Read, Write};
 use std::path::Path;
+
+type TextFuture<'a> = futures_util::future::LocalBoxFuture<'a, Result<String, Error>>;
 
 /// Provides extension methods for working with HTTP responses.
 pub trait ResponseExt<T> {
@@ -78,7 +82,7 @@ pub trait ResponseExt<T> {
     ///
     /// This method consumes the entire response body stream and can only be
     /// called once, unless you can rewind this response body.
-    fn text_async(&mut self) -> Text<'_, T>
+    fn text_async(&mut self) -> TextFuture<'_>
     where
         T: AsyncRead + Unpin;
 
@@ -119,20 +123,82 @@ impl<T> ResponseExt<T> for Response<T> {
         io::copy(self.body_mut(), &mut writer)
     }
 
+    #[cfg(feature = "encoding")]
     fn text(&mut self) -> Result<String, Error>
     where
         T: Read,
     {
-        let mut s = String::default();
-        self.body_mut().read_to_string(&mut s)?;
-        Ok(s)
+        let encoding = get_encoding(self).unwrap();
+        let mut decoder = encoding.new_decoder();
+        let mut string = String::new();
+
+        let mut buf = [0; 8192];
+
+        loop {
+            let len = match self.body_mut().read(&mut buf) {
+                Ok(0) => break,
+                Ok(len) => len,
+                Err(e) => if e.kind() == io::ErrorKind::Interrupted {
+                    continue
+                } else {
+                    return Err(e.into())
+                },
+            };
+
+            match decoder.decode_to_string(&buf[..len], &mut string, false) {
+                (encoding_rs::CoderResult::InputEmpty, _, _) => {
+
+                }
+                _ => {}
+            }
+        }
+
+        Ok(string)
     }
 
-    fn text_async(&mut self) -> Text<'_, T>
+    #[cfg(not(feature = "encoding"))]
+    fn text(&mut self) -> Result<String, Error>
+    where
+        T: Read,
+    {
+        let mut string = String::new();
+        self.body_mut().read_to_string(&mut string)?;
+
+        Ok(string)
+    }
+
+    fn text_async(&mut self) -> TextFuture<'_>
     where
         T: AsyncRead + Unpin,
     {
-        Text::new(self.body_mut())
+        Box::pin(async move {
+            let encoding = get_encoding(self).unwrap();
+            let mut decoder = encoding.new_decoder();
+            let mut string = String::new();
+
+            let mut buf = [0; 8192];
+
+            loop {
+                let len = match self.body_mut().read(&mut buf).await {
+                    Ok(0) => break,
+                    Ok(len) => len,
+                    Err(e) => if e.kind() == io::ErrorKind::Interrupted {
+                        continue
+                    } else {
+                        return Err(e.into())
+                    },
+                };
+
+                match decoder.decode_to_string(&buf[..len], &mut string, false) {
+                    (encoding_rs::CoderResult::InputEmpty, _, _) => {
+
+                    }
+                    _ => {}
+                }
+            }
+
+            Ok(string)
+        })
     }
 
     #[cfg(feature = "json")]
@@ -146,3 +212,29 @@ impl<T> ResponseExt<T> for Response<T> {
 }
 
 pub(crate) struct EffectiveUri(pub(crate) Uri);
+
+fn get_encoding<T>(response: &http::Response<T>) -> Option<encoding_rs::Encoding> {
+    let content_type = response.headers().get(http::header::CONTENT_TYPE)?;
+
+    let content_type = match content_type.to_str() {
+        Ok(s) => s,
+        Err(e) => {
+            log::warn!("could not parse Content-Type header: {}", e);
+            return None;
+        }
+    };
+
+    let content_type = match content_type.parse::<mime::Mime>() {
+        Ok(s) => s,
+        Err(e) => {
+            log::warn!("could not parse Content-Type header: {}", e);
+            return None;
+        }
+    };
+
+    .and_then(|mime| mime.get_param("charset"))
+    .map(|charset| charset.as_str().as_bytes())
+    .and_then(encoding_rs::Encoding::for_label)
+    .unwrap_or(encoding_rs::UTF_8);
+    None
+}
